ca16e5483683af7fda0285caff05a22e03b1d108
Mirko Kaempf <mirko.kaempf@bitocean.de>
2012-11-13 18:01:56 +0100

    added tutorial one on MRUnit.

---
+++ java-tutorial/nbp/MR1a/src/main/java/tutorial/mrunit/App.java
@@ -0,0 +1,13 @@
+package tutorial.mrunit;
+
+/**
+ *  Map Reduce Example 
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
